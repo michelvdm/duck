9311324d821e444c242019b2778284614364ceda
Michel Van der Meiren <michelvdmeiren@gmail.com>
2019-09-03 09:09:26 +0200

    Hide config file from git
    Hide config file

--- config/sample-config.php
+++ config/sample-config.php
@@ -1,7 +1,12 @@
 <?php defined('BASE') or die("Access denied");
 
+/* 
+	this is a sample of the mysql configuration 
+	change the properties by your mysql connection data
+*/
+
 return [
-	'dbname'=>'duck', 
+	'dbname'=>'sample', 
 	'host'=>'localhost', 
 	'user'=>'root', 
 	'password'=>''
